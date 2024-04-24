183c0c002073550cee1ff7d826dfdd3f26b588f3
Bartosz Stanczuk <synergy170185@gmail.com>
2024-04-24 09:36:04 +0000

    Add secret key

--- diner_booking_system/settings.py
+++ diner_booking_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-az61)0v8&e(1ch)a+u67%h=m*wnoar*8xlwa$-(ceoh4wr#1!2'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'bosmanfull2001barryindefermary')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
